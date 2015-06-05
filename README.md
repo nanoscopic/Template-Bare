9176c3c2b0c030c6c651ff4fab40d8f2e05866be
David Helkowski <opensource@codechild.com>
2015-06-05 11:36:47 -0400

    First version of Template::Bare

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Template-Bare
 Minimal Perl templating system
+
+Template Bare is a stripped down/optimized version of Text::Template.
+It was originally based off Text::Template version 1.46.
+
+Nearly all features and options have been removed to target the specific use case of having templates with parantheses to indicate code blocks of perl.
+
+The module supports the following two functions to fill in a template:
+1. fill_in_string( $string, $perl_hash, $package_name );
+  This function works similarly to the original fill_in_string function but takes arguments by position. The only way of passing in parameters is via a perl hash. A package name of the namespace to evaluate the template in must be provided.
+
+2. tpl_to_chunks( $string, $perl_hash, $package );
+  This function is just like fill_in_string but different in that instead of returning a flat string of the processed template it returns an array of the evaluated pieces. Additionally it supports an additional template substitution type of the format "template text {:text_param_1:text_param2:{$a}}". You can have as many text parameters as you want. $a in this example is whatever normal perl code you want. Note this format will not allow any colons within the perl code. That is a current limitation of how the code is processed.
---
+++ lib/Template/Bare.pm
@@ -0,0 +1,148 @@
+# Template::Bare - version 0.02
+package Template::Bare;
+# Originally based off Text::Template Version 1.46
+# Changes from Text::Template: Removed 'safe' option, ability to use template sources other than a plain string,
+#   'prepend' option, custom delimiters, and $ERROR variable. Mercilessly removed anything that is not absolutely
+#   needed for basic processing of text templates
+
+use Exporter;
+use Carp;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw(fill_in_string tpl_to_chunks);
+use strict;
+$Template::Bare::VERSION = '0.01';
+sub new { my $pkg = shift; return bless { @_ }, $pkg; }
+
+sub compile {
+  my $self = shift;
+  my @tokens = split /(\\\\(?=\\*[{}])|\\[{}]|[{}\n])/, $self->{'source'};
+  my $state = 0;# 0 is text, 1 is prog
+  my $depth = 0;
+  my $lineno = 1;
+  my @content;
+  my $cur_item = '';
+  my $prog_start;
+  
+  for my $t ( @tokens ) {
+    if( $t eq '{' ) {
+      if( $depth == 0 ) {
+        push( @content, [$state, $cur_item, $lineno] ) if $cur_item ne '';
+        $cur_item = '';
+        $state = 1;
+        $prog_start = $lineno;
+      }
+      else { $cur_item .= $t; }
+      $depth++;
+    }
+    elsif( $t eq '}' ) {
+      $depth--;
+      if( $depth < 0 ) { die "Unmatched close brace at line $lineno"; }
+      elsif( $depth == 0 ) {
+        push( @content, [$state, $cur_item, $prog_start] ) if $cur_item ne '';
+        $state = 0;
+        $cur_item = '';
+      }
+      else { $cur_item .= $t; }
+    }
+    elsif( $t eq "\n" ) {
+      $lineno++;
+      $cur_item .= $t;
+    }
+    else { $cur_item .= $t; }
+  }
+  
+  if( $state==1 ) { confess "End of data in template at line $prog_start -- " . $self->{'source'}; }
+  if( $state==0 ) { push( @content, [$state, $cur_item, $lineno] ) if $cur_item ne ''; }
+  
+  $self->{'source'} = \@content;
+  return $self;
+}
+
+sub fill_in {
+  my $self = shift;
+  $self->write_hash( $self->{'hash'} );
+  my $package = $self->{'package'};
+  my $r = '';
+  for my $item ( @{$self->{'source'}} ) {
+    my ( $type, $text, $lineno ) = @$item;
+    if( $type==0 ) { $r .= $text; next; }
+    my $progtext = "package $package; #line $lineno\n$text;";
+    no strict;
+    my $OUT;
+    my $res = eval $progtext;
+    my $eval_err = $@;
+    $res = $OUT if defined $OUT;
+    die $self->{'origin'} . " - Template error ``$eval_err''" if( $eval_err );
+    $r .= $res;
+  }
+  return $r;
+}
+
+sub to_chunks {
+  my $self = shift;
+  $self->write_hash( $self->{'hash'} );
+  my $package = $self->{'package'};
+  my @chunks;
+  for my $item ( @{$self->{'source'}} ) {
+    my ( $type, $text, $lineno ) = @$item;
+    if( $type==0 ) { push( @chunks, [ 0, $text ] ); next; }
+    if( $text =~ m/^\:/ ) {
+      $text = substr( $text, 1 );
+      my @parts = split( ':', $text );
+      my @out;
+      for my $part ( @parts ) {
+        if( $part =~ m/^\{(.+)\}$/ ) {
+          no strict;
+          my $OUT;
+          my $progtext = "package $package; #line $lineno\n$part;";
+          my $res = eval $progtext;
+          my $eval_err = $@;
+          $res = $OUT if defined $OUT;
+          die $self->{'origin'} . " - Template error ``$eval_err''" if( $eval_err );     
+          push( @out, $res );
+        }
+        else {
+          push( @out, $part );
+        }
+      }
+      push( @chunks, [ 1, \@out ] );
+    }
+    else {
+      my $progtext = "package $package; #line $lineno\n$text;";
+      no strict;
+      my $OUT;
+      my $res = eval $progtext;
+      my $eval_err = $@;
+      $res = $OUT if defined $OUT;
+      die $self->{'origin'} . " - Template error ``$eval_err''" if( $eval_err );     
+      push( @chunks, [ 0, $res ] );
+    }
+  }
+  return \@chunks;
+}
+
+sub write_hash {
+  my ( $self, $hash ) = @_;
+  my $package = $self->{'package'};
+  for my $name ( keys %$hash ) {
+    my $val = $hash->{$name};
+    no strict 'refs';
+    local *SYM = *{"${package}::$name"};
+    if( ! defined $val ) { delete ${"${package}::"}{$name}; } 
+    elsif( ref $val    ) { *SYM = $val; } 
+    else                 { *SYM = \$val; }
+  }
+}
+
+sub tpl_to_chunks {
+  my ( $string, $hash, $package, $more ) = @_;
+  my ( $op, $of, $ol ) = caller; $more ||= '';
+  return Template::Bare->new( source => $string, hash => $hash, package => $package, origin => "$of - $ol - $more" )->compile()->to_chunks();
+}
+
+sub fill_in_string {
+  my ( $string, $hash, $package, $more ) = @_;
+  my ( $op, $of, $ol ) = caller; $more ||= '';
+  return Template::Bare->new( source => $string, hash => $hash, package => $package, origin => "$of - $ol - $more" )->compile()->fill_in();
+}
+1;
